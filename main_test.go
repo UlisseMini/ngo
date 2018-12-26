114688c0ac6b41bd448eb5efca271122dce42397
UlisseMini <ulisse.mini@gmail.com>
2018-12-26 00:05:11 -0500

    added packet verification

--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"crypto/rand"
+	"fmt"
 	"github.com/UlisseMini/ngo/internal/aes"
 	log "github.com/sirupsen/logrus"
 	"net"
@@ -21,6 +24,7 @@ func init() {
 // Add more test cases
 // Add verification of a correct connection by transmiting a packet back and forth
 func Test_connect(t *testing.T) {
+	const packetSize = 16 // size of the packet in bytes
 	configs := []config{
 		config{
 			proto:   "tcp",
@@ -35,7 +39,19 @@ func Test_connect(t *testing.T) {
 	}
 
 	for _, conf := range configs {
-		errChan := make(chan error, 1)
+		// generate the packet
+		packet := make([]byte, packetSize)
+		n, err := rand.Read(packet)
+		if err != nil {
+			// this should never error, but just in case
+			t.Error(err)
+			continue
+		} else if n < packetSize {
+			t.Errorf("n (%d) is less then packetSize (%d)", n, packetSize)
+			continue
+		}
+
+		errChan := make(chan error)
 		go func() {
 			l, err := net.Listen(conf.proto, conf.addr)
 			if err != nil {
@@ -49,22 +65,48 @@ func Test_connect(t *testing.T) {
 				errChan <- err
 				return
 			}
-			err = conn.Close()
-			errChan <- err
+			defer conn.Close()
+
+			// read the packet
+			buf := make([]byte, packetSize)
+			n, err := conn.Read(buf)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			if n < packetSize {
+				errChan <- fmt.Errorf("server: n(%d) is less then packetSize(%d)",
+					n, packetSize)
+				return
+			}
+			// now make sure its valid
+			if bytes.Compare(buf, packet) != 0 {
+				errChan <- fmt.Errorf("server: buf (%X) != packet (%X)", buf, packet)
+			}
 		}()
 
 		select {
 		case err := <-errChan:
-			t.Fatalf("server: %v", err)
+			t.Fatalf("server returned: %v", err)
 		default:
 			// give the listener time to get ready (should really find better way ;p
 			time.Sleep(10 * time.Millisecond)
 
 			conn, err := connect(conf)
 			if err != nil {
-				t.Fatalf("client: %v", err)
+				t.Fatalf("connect returned: %v", err)
 			}
 			defer conn.Close()
+
+			// now we have the connection send the packet
+			n, err := conn.Write(packet)
+			if err != nil {
+				t.Fatalf("failed to send packet: %v", err)
+			}
+			if n < packetSize {
+				t.Fatalf("n (%d) < packetSize (%d)", n, packetSize)
+			}
+			// TODO listen for the packet back
 		}
 	}
 }
